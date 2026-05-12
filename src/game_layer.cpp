e2efb21a1a137f729b07f9a06f0239a64885d0b4
qiekn <qiekndev@gmail.com>
2026-05-13 07:45:12 +0800

    fix(text): use ck::DrawText so default font renders Chinese glyphs

--- src/game_layer.cpp
+++ src/game_layer.cpp
@@ -3,14 +3,16 @@
 #include <imgui.h>
 #include <raylib.h>
 
+#include "font_default.hpp"
+
 namespace ck {
 
 void GameLayer::OnRender() {
   const char* title = "Block · 方块";
   const int font_size = 48;
-  const int tw = MeasureText(title, font_size);
-  DrawText(title, (GetScreenWidth() - tw) / 2, (GetScreenHeight() - font_size) / 2, font_size,
-           BLACK);
+  const int tw = ck::MeasureText(title, font_size);
+  ck::DrawText(title, (GetScreenWidth() - tw) / 2, (GetScreenHeight() - font_size) / 2, font_size,
+               BLACK);
 }
 
 void GameLayer::OnImGuiRender() {
